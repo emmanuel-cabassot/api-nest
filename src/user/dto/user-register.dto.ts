6c19a7983ae42f6b9ff6ac2997088ff7842a2e30
emmanuel-cabassot <emmanuel.cabassot@laplateforme.io>
2022-12-07 13:15:56 +0100

    jwt-secret et refresh-jwt-secret en base 64

--- src/user/dto/user-register.dto.ts
+++ src/user/dto/user-register.dto.ts
@@ -1,3 +1,4 @@
+import { UserRoleEnum } from './../../enum/user-role.enum';
 import { IsString, IsNotEmpty, IsNumber, Min, Max, IsEmail, IsOptional } from 'class-validator';
 import { Type } from 'class-transformer';
 
@@ -13,4 +14,7 @@ export class userRegisterDto {
     @IsNotEmpty()
     @IsEmail()
     email: string;
+
+    @IsOptional()
+    role: UserRoleEnum;
 }
